898d999280389158f9df3151d72c90a346cb7ba8
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-10-27 17:09:14 +0200

    lab1 done

---
+++ modern-langs-lab1/modern-langs-lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modern_langs_lab1
+{
+    class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            if (name.Equals(""))
+            {
+                Console.WriteLine("Hello World, stranger greets you");
+            }
+            else
+            {
+                Console.WriteLine("Hello world, author of this app is " + name + "!");
+            }
+        }
+
+        static void Task2()
+        {
+            DateTime today = DateTime.Now;
+            DateTime birsday = new DateTime(2019, 6, 5);
+            Console.WriteLine(birsday.Subtract(today).Days);
+        }
+
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+        }
+    }
+}
